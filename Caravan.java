482c90ae15d6a1d51969fed508cfe72232769ebc
Alejandro Cortes <efrain.alejandro.cortes@gmail.com>
2018-09-29 14:41:07 -0400

    Working MST

--- Caravan.java
+++ Caravan.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-import java.util.*;
+import java.util.*;	
+import java.util.PriorityQueue;
+import java.util.Comparator;
 
-	
 
 public class Caravan{	
 
@@ -11,7 +12,127 @@ public class Caravan{
 	public static int wagonCost; // wagon cost
 	public static int shipWeight; // Total weight of a complete shipment
 
+	public static class Edge implements Comparable<Edge>
+	{
+		int source, destination, roadCost, weightCap;
+
+		Edge(int source, int destination, int roadCost, int weightCap){
+			this.source = source;
+			this.destination = destination;
+			this.roadCost = roadCost;
+			this.weightCap = weightCap;
+		}
+
+		public int compareTo(Edge o)
+		{
+			return Integer.compare(roadCost, o.roadCost);
+		}
+
+		public String toString()
+		{
+			return "(" + this.source + ", " + this.destination + ", " + this.roadCost + ", " + this.weightCap + ")";
+		}
+	}
+	// A class to represent a subset for union-find
+	class subset
+	{
+		int parent, rank;
+	}
+
+	int V, E;
+	Edge edge[];
+
+	public static class Graph
+	{
+		int vertices;
+		ArrayList<Edge> allEdges = new ArrayList<>();
+
+		Graph(int vertices)
+		{
+			this.vertices = vertices;
+		}
+
+		public void addEdge(int source, int destination, int roadCost, int weightCap)
+		{
+			Edge edge = new Edge(source, destination, roadCost, weightCap);
+			allEdges.add(edge);
+		}
+
+		public void kruskalMST()
+		{
+			PriorityQueue<Edge> pq = new PriorityQueue<>(allEdges.size(), Comparator.comparingInt(o -> o.roadCost));
+
+			for(int i = 0; i < allEdges.size(); i++)
+			{	
+				// System.out.println(allEdges.get(i));
+				pq.add(allEdges.get(i));
+			}			
+
+			int [] parent = new int[vertices];
+
+			makeSet(parent);
+
+			ArrayList<Edge> mst = new ArrayList<>();
+
+			int index = 0;
+
+			while(index < vertices - 1)
+			{
+				Edge edge = pq.remove();
+				int x_set = find(parent, edge.source);
+				int y_set = find(parent, edge.destination);
+
+				if(x_set == y_set){
+					// Ignore contains both
+				}else{
+					mst.add(edge);
+					index++;
+					union(parent, x_set, y_set);
+				}
+			}
+
+			System.out.println("\nMST: ");
+			printGraph(mst);
+		
+
+		}
+
+		public void makeSet(int [] parent)
+		{
+			for(int i = 0; i < vertices; i++)
+				parent[i] = i;
+		}
+
+		// Function that finds if node already exists in tree
+		public int find(int [] parent, int vertex)
+		{
+			if(parent[vertex] != vertex)
+				return find(parent, parent[vertex]);
+			return vertex;
+		}
+
+		// Function that that does the union of two sets
+		void union(int [] parent, int x, int y)
+		{
+			int x_set_parent = find(parent, x);
+			int y_set_parent = find(parent, y);
+
+			parent[y_set_parent] = x_set_parent;
+
+		}
 	
+		// Output function
+		public void printGraph(ArrayList<Edge> edgeList)
+		{
+			for(int i = 0; i < edgeList.size(); i++)
+			{
+				Edge edge = edgeList.get(i);
+				System.out.println(edge);
+			}
+		}
+
+	// end of graph 		
+	}
 
 	public static void main(String[] args)
 	{
@@ -19,12 +140,11 @@ public class Caravan{
 
 		// First line scans n & m
 		// n = number of cities
-		
+		// m = number of roads
 		n = sc.nextInt();
 		m = sc.nextInt();
 
-		
-
+		Graph graph = new Graph(n);
 
 		ArrayList<Edge>[] adjList = new ArrayList[n];
 		for(int i = 0; i < n; i++)
@@ -53,51 +173,19 @@ public class Caravan{
 			Edge e1 = new Edge( A, B, C, W);
 
 			adjList[A].add(e1);
+			graph.addEdge(A, B, C, W);
 		}
 
 		budget = sc.nextInt();
 		wagonCost = sc.nextInt();
 		shipWeight = sc.nextInt();
 
-		System.out.println("\n[Un]Sorted Edges:");
-		for(ArrayList<Edge> al : adjList)
-		{
-			for(Edge e : al)
-				System.out.println(e);
-		}
+		graph.kruskalMST();
+
+
 		// First line of output should contain k
 		// where k is the number of possible vehicle counts that can enable the transportation of shipment in one trip
-
 		// Second line of output should contain k distinct integers between [1, 10]
-
-		//
-	}
-
-	public static class Edge implements Comparable<Edge>
-	{
-		int source, destination, roadCost, weightCap;
-
-		Edge(int source, int destination, int roadCost, int weightCap){
-			this.source = source;
-			this.destination = destination;
-			this.roadCost = roadCost;
-			this.weightCap = weightCap;
-		}
-
-		public int compareTo(Edge o)
-		{
-			return Integer.compare(roadCost, o.roadCost);
-		}
-
-		public String toString()
-		{
-			return "(" + this.source + ", " + this.destination + ", " + this.roadCost + ", " + this.weightCap + ")";
-		}
-	}
-
-	public static void getEdges()
-	{
-
 	}
 }
 
@@ -112,8 +200,23 @@ public class Caravan{
 2 4 1 5
 27 1 51
 
---------------------- Sample Output ---------------------
-1
+Case 1:
+27 1 51
+1 
 9
+
+Case 2:
+41 1 55
+2
+6 10
+
+Case 3:
+71 6 49
+3
+5 6 10
+
 */
 
+// Sort them based on cost
+// add them to MST starting with lowest cost first
+// but if the carry capacity of the road is less than the max carry capacity, skip it also
